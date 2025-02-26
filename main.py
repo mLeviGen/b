8e5725979adbb0c27e51b3acd0fc793869d01f5d
mLeviGen <79418518+mLeviGen@users.noreply.github.com>
2025-02-27 00:29:15 +0200

    updated

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import time
 
-print("Зaadawdadwadawaапуск долгоживущего процесса. Нажмите Ctrl+C для остановки.")
+print("Запуск долгоживущего процесса. Нажмите Ctrl+C для остановки.")
 
 counter = 0
 try:
